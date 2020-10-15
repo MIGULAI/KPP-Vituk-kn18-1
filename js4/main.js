241398c5221ad325b01bc26d697d90b254b26b40
IvanVitiuk <vitukivan05@gmail.com>
2020-10-15 20:53:56 +0300

    add

--- js4/main.js
+++ js4/main.js
@@ -1,29 +1,40 @@
-let i = 0;
+let element = [];
 
 function Add(){
     let newDeal = document.getElementById("add").value;
     let table = document.getElementById("table");
     let newStrin = document.createElement("div");
     let newSubnode1 = document.createElement("div");
-    let newSubnode2 = document.createElement("div");
+    let newSubnode2 = document.createElement("input");
+
+    newSubnode2.type = "checkbox";
+    newSubnode2.addEventListener("click" , function(){
+        newSubnode2.parentNode.parentNode.removeChild(newSubnode2.parentNode);
+    })
 
     newStrin.id = "strin";
     newSubnode1.id = "subnode";
     newSubnode2.id = "subnode";
     
     newSubnode1.textContent = newDeal;
-    localStorage.setItem(i.toString() , newDeal);
     
     table.appendChild(newStrin);
     newStrin.appendChild(newSubnode1);
     newStrin.appendChild(newSubnode2);
+
+    saveToStorage(newDeal);
 }
 
 function AddAll(a){
     let table = document.getElementById("table");
     let newStrin = document.createElement("div");
     let newSubnode1 = document.createElement("div");
-    let newSubnode2 = document.createElement("div");
+    let newSubnode2 = document.createElement("input");
+
+    newSubnode2.type = "checkbox";
+    newSubnode2.addEventListener("click" , function(){
+        newSubnode2.parentNode.parentNode.removeChild(newSubnode2.parentNode);
+    })
 
     newStrin.id = "strin";
     newSubnode1.id = "subnode";
@@ -36,17 +47,15 @@ function AddAll(a){
     newStrin.appendChild(newSubnode2);
 }
 
+function saveToStorage(newDeal){
+    element.push(newDeal);
+    var serial = JSON.stringify(element);
+    localStorage.setItem("one",serial);
+}
+
 function init(){
-    let status = true;
-    while(status){
-        let a = localStorage.getItem(i.toString())
-        i++;
-        if(a === null){
-            status = false;
-            break;
-        }
-        else{
-            AddAll(a);
-        }
+    element = JSON.parse(localStorage.getItem("one"));
+    for(let i = 0 ; i < element.length ; i++){
+        AddAll(element[i]);
     }
 }
